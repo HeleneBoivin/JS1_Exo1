ef8c0c022924536bbebd7ecc4597b0584fd47a18
Boivin <boivinhelene@yahoo.com>
2018-10-12 13:37:06 +0200

    JS Partie 1

---
+++ Assets/JS/javascript.js
@@ -0,0 +1,8 @@
+// 1ière solution, déclaration de la variable name initialisé avec une chaine de caractère (string)
+var name = "Hélène";
+alert(name);
+/*
+2ième solution affichage de la variable name dans une boite de dialogue
+que l'on appelle avec la fonction alert
+ alert("Hélène")
+*/
